e3b20b8669e4d495c4275109393d1c2288b3ca2d
Martin Erpicum <martin@erpicum.net>
2021-04-19 14:00:11 +0200

    add cache for homepage

--- app/app/Http/Resources/HomePageResource.php
+++ app/app/Http/Resources/HomePageResource.php
@@ -5,24 +5,30 @@ namespace App\Http\Resources;
 use App\Models\Court;
 use App\Models\Document;
 use URL;
+use Cache;
 
 class HomePageResource extends BaseResource
 {
     public function toArray($request)
     {
         return [
-
             'title' => "A free open source solution developed by OpenJustice.be to browse Belgian case law.",
             'recent_documents' => [
                 'title' => 'Recent documents',
-                'documents' => DocumentMinimalResource::collection(Document::orderBy('created_at', 'desc')->limit(10)->get()),
+                'documents' => Cache::remember('recent_documents', 3600, function () {
+                    return DocumentMinimalResource::collection(Document::orderBy('created_at', 'desc')->limit(10)->get());
+                }),
             ],
             'court_categories' => [
                 'title' => 'Browse categories and courts',
                 'categories' => CategoryResource::collection($this),
             ],
-            'count_documents' => Document::count(),
-            'count_courts' => Court::count(),
+            'count_documents' => Cache::remember('count_documents', 3600 * 100, function () {
+                return Document::count();
+            }),
+            'count_courts' => Cache::remember('count_courts', 3600 * 100, function () {
+                return Court::count();
+            }),
             'links' => [
                 'self' => URL::current(),
             ],
